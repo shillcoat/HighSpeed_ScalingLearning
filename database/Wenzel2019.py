29d59760ebb73e14ecdbadc5ae34b6a687c8575d
shillcoat <sophie.hillcoat@gmail.com>
2026-01-06 18:44:36 -0800

    Unified format for Wenzel 2019 cases and plotted velocity transforms

---
+++ database/Wenzel2019.py
@@ -0,0 +1,120 @@
+"""
+Script to read in data files from Wenzel et al. (2019) and convert to database format
+
+---------------------------------------------------------------------------------------------------
+
+C. Wenzel, T. Gibis, M. Kloker, and U. Rist, “Self-similar compressible turbulent boundary layers with pressure gradients. 
+Part 1. Direct numerical simulation and assessment of Morkovin’s hypothesis,” Journal of Fluid Mechanics, 
+vol. 880, pp. 239–283, Dec. 2019, doi: 10.1017/jfm.2019.670.
+"""
+
+# %% Imports
+import numpy as np
+from glob import glob
+
+from db_config import BL, save_case
+from filepaths import Wenzel2019_path  # This file is not kept in repo
+
+# %% Fetch and iterate through cases
+cases = glob(Wenzel2019_path + "/data.npy/*.npy")
+for c in cases:
+    cname = c.split('/')[-1][:-4]
+    dbCase = BL('si', incomp=0, chem=0, gamma=1.4, Pr=0.71, R=287.0)
+    dat = np.load(c, allow_pickle=True)[()]
+
+    dbCase.x = dat['geometry']['x']
+    dbCase.y = dat['geometry']['y']
+
+    dbCase.u = dat['2D_ReynoldsMean']['UMean']
+    dbCase.v = dat['2D_ReynoldsMean']['VMean']
+    dbCase.w = dat['2D_ReynoldsMean']['WMean']
+    dbCase.a = dat['2D_ReynoldsMean']['aMean']
+    dbCase.M = dat['2D_ReynoldsMean']['mMean']
+    dbCase.rho = dat['2D_ReynoldsMean']['rhoMean']
+    dbCase.P = dat['2D_ReynoldsMean']['pMean']
+    dbCase.T = dat['2D_ReynoldsMean']['TMean']
+    dbCase.mu = dat['2D_ReynoldsMean']['muMean']
+    dbCase.nu = dat['2D_ReynoldsMean']['nuMean']
+
+    dbCase.u_F = dat['2D_FavreMean']['UFavreMean']
+    dbCase.v_F = dat['2D_FavreMean']['VFavreMean']
+    dbCase.w_F = dat['2D_FavreMean']['WFavreMean']
+    dbCase.rho_F = dat['2D_FavreMean']['rhoFavreMean']
+    dbCase.T_F = dat['2D_FavreMean']['TFavreMean']
+    dbCase.P_F = dat['2D_FavreMean']['pFavreMean']
+    dbCase.mu_F = dat['2D_FavreMean']['muFavreMean']
+
+    dbCase.upup = dat['2D_ReynoldsFlucts']['uI_uI']
+    dbCase.vpvp = dat['2D_ReynoldsFlucts']['vI_vI']
+    dbCase.wpwp = dat['2D_ReynoldsFlucts']['wI_wI']
+    dbCase.rhoprhop = dat['2D_ReynoldsFlucts']['rI_rI']
+    dbCase.TpTp = dat['2D_ReynoldsFlucts']['TI_TI']
+    dbCase.PpPp = dat['2D_ReynoldsFlucts']['pI_pI']
+    dbCase.upvp = dat['2D_ReynoldsFlucts']['uI_vI']
+    dbCase.upwp = dat['2D_ReynoldsFlucts']['uI_wI']
+    dbCase.vpwp = dat['2D_ReynoldsFlucts']['vI_wI']
+    dbCase.upTp = dat['2D_ReynoldsFlucts']['uI_TI']
+    dbCase.vpTp = dat['2D_ReynoldsFlucts']['vI_TI']
+    dbCase.wpTp = dat['2D_ReynoldsFlucts']['wI_TI']
+
+    dbCase.ruppupp = dat['2D_FavreFlucts']['r_uII_uII']
+    dbCase.rvppvpp = dat['2D_FavreFlucts']['r_vII_vII']
+    dbCase.rwppwpp = dat['2D_FavreFlucts']['r_wII_wII']
+    dbCase.rrhopprhopp = dat['2D_FavreFlucts']['r_rII_rII']
+    dbCase.rTppTpp = dat['2D_FavreFlucts']['r_TII_TII']
+    dbCase.rPppPpp = dat['2D_FavreFlucts']['r_pII_pII']
+    dbCase.ruppvpp = dat['2D_FavreFlucts']['r_uII_vII']
+    dbCase.ruppwpp = dat['2D_FavreFlucts']['r_uII_wII']
+    dbCase.rvppwpp = dat['2D_FavreFlucts']['r_vII_wII']
+    dbCase.ruppTpp = dat['2D_FavreFlucts']['r_uII_TII']
+    dbCase.rvppTpp = dat['2D_FavreFlucts']['r_vII_TII']
+    dbCase.rwppTpp = dat['2D_FavreFlucts']['r_wII_TII']
+
+    dbCase.tauw = dat['wallProperties']['tau_wall']
+    dbCase.utau = dat['wallProperties']['utau']
+    dbCase.Cf = dat['wallProperties']['cf']
+
+    dbCase.delta99 = dat['boundaryLayerThicknesses']['delta99']
+    dbCase.delta1 = dat['boundaryLayerThicknesses']['delta1']
+    dbCase.delta2 = dat['boundaryLayerThicknesses']['theta']
+    dbCase.H = dat['boundaryLayerThicknesses']['H12']
+    dbCase.delta1k = dat['boundaryLayerThicknesses']['delta1Inc']
+
+    dbCase.uinf = dat['boundaryLayerEdgeValues']['U_edge']
+    dbCase.rhoinf = dat['boundaryLayerEdgeValues']['rho_edge']
+    dbCase.Pinf = dat['boundaryLayerEdgeValues']['p_edge']
+    dbCase.Tinf = dat['boundaryLayerEdgeValues']['T_edge']
+    dbCase.Minf = dat['boundaryLayerEdgeValues']['m_edge']
+    dbCase.muinf = dat['boundaryLayerEdgeValues']['nu_edge']*dbCase.rhoinf
+    dbCase.uinf_F = dat['boundaryLayerEdgeValues']['UFavre_edge']
+    dbCase.rhoinf_F = dat['boundaryLayerEdgeValues']['rhoFavre_edge']
+    dbCase.Tinf_F = dat['boundaryLayerEdgeValues']['TFavre_edge']
+    dbCase.Pinf_F = dat['boundaryLayerEdgeValues']['pFavre_edge']
+    dbCase.muinf_F = dat['boundaryLayerEdgeValues']['muFavre_edge']
+
+    dbCase.Redelta99 = dat['ReynoldsNumbers']['Re_delta']
+    dbCase.Redelta1 = dat['ReynoldsNumbers']['Re_delta1']
+    dbCase.Retheta = dat['ReynoldsNumbers']['Re_theta']
+    dbCase.Retau = dat['ReynoldsNumbers']['Re_tau']
+
+    # Some extra wall quantities
+    dbCase.rhow = dbCase.rho[:,0]
+    dbCase.muw = dbCase.mu[:,0]
+    dbCase.Tw = dbCase.T[:,0]
+
+    dbCase.Tr = dbCase.Tinf * (1 + 0.89*(dbCase.gamma-1.0)/2.0*dbCase.Minf**2)
+    if cname[1] == 'A': dbCase.Bk = float(cname[-4:])
+    elif cname[1] == 'F': dbCase.Bk = float(cname[-5:])
+    else: dbCase.Bk = 0
+
+    # Slice streamwise coordinate array to that which corresponds to available data
+    Nx = 5000 if dbCase.Minf[0]>1.0 else 2500
+    dbCase.x = dbCase.x[Nx-len(dbCase.u):-200]
+
+    # Compute streamwise velocity profile in incompressible wall coordinates
+    dbCase.uplus = dbCase.u/np.transpose([dbCase.utau])
+    dbCase.deltaplus = dbCase.nu[:,0]/dbCase.utau
+    dbCase.yplus = dbCase.y/np.transpose([dbCase.deltaplus])
+
+    save_case(dbCase, Wenzel2019_path + f"/{cname}.dill")
+# %%
--- vel_transforms.py
+++ vel_transforms.py
@@ -16,30 +16,43 @@ import database.filepaths as fpath
 
 # %% Load cases
 Cmin, Cmax = 1e6, -1e6
-Cquant = "Bq"
+Cquant = "Bk"
 Sillero2014 = db.load_case(f"{fpath.Sillero2014_path}/Re_theta5000.dill")
-Zhang2018 = {}
-for c in glob(f"{fpath.Zhang2018_path}/*.dill"):
+# Zhang2018 = {}
+# for c in glob(f"{fpath.Zhang2018_path}/*.dill"):
+#     cname = c.split('/')[-1][:-5]
+#     Zhang2018[cname] = db.load_case(c)
+#     Cq = getattr(Zhang2018[cname],Cquant)
+#     Cmin = Cq if Cq<Cmin else Cmin
+#     Cmax = Cq if Cq>Cmax else Cmax
+#     _ = Zhang2018[cname].vel_transform(label="VD")
+#     _ = Zhang2018[cname].vel_transform(label="TL")
+#     _ = Zhang2018[cname].vel_transform(label="V")
+#     _ = Zhang2018[cname].vel_transform(label="GFM")
+# Volpiani2020 = {}; Vxs = 900
+# for c in glob(f"{fpath.Volpiani2020_path}/*.dill"):
+#     cname = c.split('/')[-1][:-5]
+#     Volpiani2020[cname] = db.load_case(c)
+#     Cq = getattr(Volpiani2020[cname],Cquant)[Vxs]
+#     Cmin = Cq if Cq<Cmin else Cmin
+#     Cmax = Cq if Cq>Cmax else Cmax
+#     _ = Volpiani2020[cname].vel_transform(label="VD")
+#     _ = Volpiani2020[cname].vel_transform(label="TL")
+#     _ = Volpiani2020[cname].vel_transform(label="V")
+#     _ = Volpiani2020[cname].vel_transform(label="GFM")
+Wenzel2019 = {}; Retau_plt = 490
+for c in glob(f"{fpath.Wenzel2019_path}/*.dill"):
     cname = c.split('/')[-1][:-5]
-    Zhang2018[cname] = db.load_case(c)
-    Cq = getattr(Zhang2018[cname],Cquant)
+    dat = db.load_case(c)
+    if Retau_plt>dat.Retau[-1] or Retau_plt<dat.Retau[0]: continue  # Outside of range of Retau covered by data
+    Wenzel2019[cname] = dat
+    Cq = getattr(Wenzel2019[cname],Cquant)
     Cmin = Cq if Cq<Cmin else Cmin
     Cmax = Cq if Cq>Cmax else Cmax
-    _ = Zhang2018[cname].vel_transform(label="VD")
-    _ = Zhang2018[cname].vel_transform(label="TL")
-    _ = Zhang2018[cname].vel_transform(label="V")
-    _ = Zhang2018[cname].vel_transform(label="GFM")
-Volpiani2020 = {}; Vxs = 900
-for c in glob(f"{fpath.Volpiani2020_path}/*.dill"):
-    cname = c.split('/')[-1][:-5]
-    Volpiani2020[cname] = db.load_case(c)
-    Cq = getattr(Volpiani2020[cname],Cquant)[Vxs]
-    Cmin = Cq if Cq<Cmin else Cmin
-    Cmax = Cq if Cq>Cmax else Cmax
-    _ = Volpiani2020[cname].vel_transform(label="VD")
-    _ = Volpiani2020[cname].vel_transform(label="TL")
-    _ = Volpiani2020[cname].vel_transform(label="V")
-    _ = Volpiani2020[cname].vel_transform(label="GFM")
+    _ = Wenzel2019[cname].vel_transform(label="VD")
+    _ = Wenzel2019[cname].vel_transform(label="TL")
+    _ = Wenzel2019[cname].vel_transform(label="V")
+    _ = Wenzel2019[cname].vel_transform(label="GFM")
 
 # %% Plotting
 col = lambda q: cm.rainbow((q-Cmin)/(Cmax-Cmin))
@@ -48,12 +61,18 @@ cmap = cm.ScalarMappable(matplotlib.colors.Normalize(vmin=Cmin, vmax=Cmax),"rain
 # No compressible scaling
 fig, ax = plt.subplots(figsize=[6,4])
 ax.grid(which='both', color='0.9')
-for c in Zhang2018.keys():
-    ax.semilogx(Zhang2018[c].yplus, Zhang2018[c].uplus, 
-                color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
-for c in Volpiani2020.keys():
-    ax.semilogx(Volpiani2020[c].yplus[Vxs], Volpiani2020[c].uplus[Vxs], '--',
-                color=col(getattr(Volpiani2020[c],Cquant)[Vxs]), label=f"V20:{c}")
+# for c in Zhang2018.keys():
+#     ax.semilogx(Zhang2018[c].yplus, Zhang2018[c].uplus, 
+#                 color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
+# for c in Volpiani2020.keys():
+#     ax.semilogx(Volpiani2020[c].yplus[Vxs], Volpiani2020[c].uplus[Vxs], '--',
+#                 color=col(getattr(Volpiani2020[c],Cquant)[Vxs]), label=f"V20:{c}")
+for c in Wenzel2019.keys():
+    xi = np.where(Wenzel2019[c].Retau>=Retau_plt)[0][0]
+    lstyle = '-' if Wenzel2019[c].Minf[0]>1.0 else '--'
+    ax.semilogx(Wenzel2019[c].yplus[xi], Wenzel2019[c].uplus[xi], lstyle,
+                color=col(getattr(Wenzel2019[c],Cquant)), label=f"W19:{c}",
+                alpha=1.0 if Wenzel2019[c].Minf[0]>1.0 else 0.3)
 ax.semilogx(Sillero2014.yplus, Sillero2014.uplus, color='black', 
             label="Sillero et al. (2014)")
 # ax.legend(loc='best', framealpha=1)
@@ -66,12 +85,18 @@ fig.tight_layout(); fig.show()
 # Van Driest
 fig, ax = plt.subplots(figsize=[6,4])
 ax.grid(which='both', color='0.9')
-for c in Zhang2018.keys():
-    ax.semilogx(Zhang2018[c].yplusVD, Zhang2018[c].uplusVD, 
-                color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
-for c in Volpiani2020.keys():
-    ax.semilogx(Volpiani2020[c].yplusVD[Vxs], Volpiani2020[c].uplusVD[Vxs], '--',
-                color=col(getattr(Volpiani2020[c],Cquant)[Vxs]), label=f"V20:{c}")
+# for c in Zhang2018.keys():
+#     ax.semilogx(Zhang2018[c].yplusVD, Zhang2018[c].uplusVD, 
+#                 color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
+# for c in Volpiani2020.keys():
+#     ax.semilogx(Volpiani2020[c].yplusVD[Vxs], Volpiani2020[c].uplusVD[Vxs], '--',
+#                 color=col(getattr(Volpiani2020[c],Cquant)[Vxs]), label=f"V20:{c}")
+for c in Wenzel2019.keys():
+    xi = np.where(Wenzel2019[c].Retau>=Retau_plt)[0][0]
+    lstyle = '-' if Wenzel2019[c].Minf[0]>1.0 else '--'
+    ax.semilogx(Wenzel2019[c].yplusVD[xi], Wenzel2019[c].uplusVD[xi], lstyle,
+                color=col(getattr(Wenzel2019[c],Cquant)), label=f"W19:{c}",
+                alpha=1.0 if Wenzel2019[c].Minf[0]>1.0 else 0.3)
 ax.semilogx(Sillero2014.yplus, Sillero2014.uplus, color='black', 
             label="Sillero et al. (2014)")
 # ax.legend(loc='best', framealpha=1)
@@ -84,12 +109,18 @@ fig.tight_layout(); fig.show()
 # Trettel & Larsson
 fig, ax = plt.subplots(figsize=[6,4])
 ax.grid(which='both', color='0.9')
-for c in Zhang2018.keys():
-    ax.semilogx(Zhang2018[c].yplusTL, Zhang2018[c].uplusTL, 
-                color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
-for c in Volpiani2020.keys():
-    ax.semilogx(Volpiani2020[c].yplusTL[Vxs], Volpiani2020[c].uplusTL[Vxs], '--',
-                color=col(getattr(Volpiani2020[c],Cquant)[Vxs]), label=f"V20:{c}")
+# for c in Zhang2018.keys():
+#     ax.semilogx(Zhang2018[c].yplusTL, Zhang2018[c].uplusTL, 
+#                 color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
+# for c in Volpiani2020.keys():
+#     ax.semilogx(Volpiani2020[c].yplusTL[Vxs], Volpiani2020[c].uplusTL[Vxs], '--',
+#                 color=col(getattr(Volpiani2020[c],Cquant)[Vxs]), label=f"V20:{c}")
+for c in Wenzel2019.keys():
+    xi = np.where(Wenzel2019[c].Retau>=Retau_plt)[0][0]
+    lstyle = '-' if Wenzel2019[c].Minf[0]>1.0 else '--'
+    ax.semilogx(Wenzel2019[c].yplusTL[xi], Wenzel2019[c].uplusTL[xi], lstyle,
+                color=col(getattr(Wenzel2019[c],Cquant)), label=f"W19:{c}",
+                alpha=1.0 if Wenzel2019[c].Minf[0]>1.0 else 0.3)
 ax.semilogx(Sillero2014.yplus, Sillero2014.uplus, color='black', 
             label="Sillero et al. (2014)")
 # ax.legend(loc='best', framealpha=1)
@@ -102,12 +133,18 @@ fig.tight_layout(); fig.show()
 # Volpiani et al.
 fig, ax = plt.subplots(figsize=[6,4])
 ax.grid(which='both', color='0.9')
-for c in Zhang2018.keys():
-    ax.semilogx(Zhang2018[c].yplusV, Zhang2018[c].uplusV, 
-                color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
-for c in Volpiani2020.keys():
-    ax.semilogx(Volpiani2020[c].yplusV[Vxs], Volpiani2020[c].uplusV[Vxs], '--', 
-                color=col(getattr(Volpiani2020[c],Cquant)[Vxs]), label=f"V20:{c}")
+# for c in Zhang2018.keys():
+#     ax.semilogx(Zhang2018[c].yplusV, Zhang2018[c].uplusV, 
+#                 color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
+# for c in Volpiani2020.keys():
+#     ax.semilogx(Volpiani2020[c].yplusV[Vxs], Volpiani2020[c].uplusV[Vxs], '--', 
+#                 color=col(getattr(Volpiani2020[c],Cquant)[Vxs]), label=f"V20:{c}")
+for c in Wenzel2019.keys():
+    xi = np.where(Wenzel2019[c].Retau>=Retau_plt)[0][0]
+    lstyle = '-' if Wenzel2019[c].Minf[0]>1.0 else '--'
+    ax.semilogx(Wenzel2019[c].yplusV[xi], Wenzel2019[c].uplusV[xi], lstyle,
+                color=col(getattr(Wenzel2019[c],Cquant)), label=f"W19:{c}",
+                alpha=1.0 if Wenzel2019[c].Minf[0]>1.0 else 0.3)
 ax.semilogx(Sillero2014.yplus, Sillero2014.uplus, color='black', 
             label="Sillero et al. (2014)")
 # ax.legend(loc='best', framealpha=1)
@@ -120,12 +157,18 @@ fig.tight_layout(); fig.show()
 # Griffin et al.
 fig, ax = plt.subplots(figsize=[6,4])
 ax.grid(which='both', color='0.9')
-for c in Zhang2018.keys():
-    ax.semilogx(Zhang2018[c].yplusGFM, Zhang2018[c].uplusGFM, 
-                color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
-for c in Volpiani2020.keys():
-    ax.semilogx(Volpiani2020[c].yplusGFM[Vxs], Volpiani2020[c].uplusGFM[Vxs], '--', 
-                color=col(getattr(Volpiani2020[c],Cquant)[Vxs]),label=f"V20:{c}")
+# for c in Zhang2018.keys():
+#     ax.semilogx(Zhang2018[c].yplusGFM, Zhang2018[c].uplusGFM, 
+#                 color=col(getattr(Zhang2018[c],Cquant)), label=f"Z18:{c}")
+# for c in Volpiani2020.keys():
+#     ax.semilogx(Volpiani2020[c].yplusGFM[Vxs], Volpiani2020[c].uplusGFM[Vxs], '--', 
+#                 color=col(getattr(Volpiani2020[c],Cquant)[Vxs]),label=f"V20:{c}")
+for c in Wenzel2019.keys():
+    xi = np.where(Wenzel2019[c].Retau>=Retau_plt)[0][0]
+    lstyle = '-' if Wenzel2019[c].Minf[0]>1.0 else '--'
+    ax.semilogx(Wenzel2019[c].yplusGFM[xi], Wenzel2019[c].uplusGFM[xi], lstyle,
+                color=col(getattr(Wenzel2019[c],Cquant)), label=f"W19:{c}",
+                alpha=1.0 if Wenzel2019[c].Minf[0]>1.0 else 0.3)
 ax.semilogx(Sillero2014.yplus, Sillero2014.uplus, color='black', 
             label="Sillero et al. (2014)")
 # ax.legend(loc='best', framealpha=1)
